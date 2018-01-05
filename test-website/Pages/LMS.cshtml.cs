b83feb2a8751f24f08c7716e71f91cb5a3894b33
Glyn Humphreys <Glyn@GalaArch.com>
2018-01-05 12:59:11 +0000

    Added a LMS page to the website, begining to put together the framework.

---
+++ test-website/Pages/LMS.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace test_website.Pages
+{
+    public class LMSModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "The main work horse of this website.";
+        }
+    }
+}
